872bb1602dfbccfecca11a66c52449258d4a52fa
Ben John <bj9@illinois.edu>
2019-04-17 18:49:36 -0500

    Added constructors to obstacle

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -1,2 +1,13 @@
 #include "obstacle.h"
 
+obstacle::obstacle(ofVec2f v1)
+{
+	top_corner = v1;
+}
+
+obstacle::obstacle()
+{
+	ofVec2f v1;
+	v1.set(ofGetWindowWidth() / 2, 100); // Preset this value to 
+	top_corner = v1;
+}
--- src/obstacle.h
+++ src/obstacle.h
@@ -2,5 +2,8 @@
 #include "ofMain.h"
 
 class obstacle {
-
+	ofVec2f top_corner; // Tracking the top left corner for bots
+public:
+	obstacle(); // Constructor given starting top left corner
+	obstacle(ofVec2f v1); // Constructor given starting top left corner
 };
